5a47591f714bd7fc4de93adba33d892e38a3640c
Vinícius Darlan <viniciuschegouoperacional@gmail.com>
2025-02-26 23:20:18 -0300

    Update home.py

--- home.py
+++ home.py
@@ -1,5 +1,12 @@
 import streamlit as st
 from datetime import datetime
+import os
+
+# Configurar o servidor programaticamente
+if "PORT" in os.environ:
+    port = int(os.environ["PORT"])
+    st.set_option('server.port', port)
+    st.set_option('server.address', '0.0.0.0')
 
 # Configuração da página inicial
 st.set_page_config(
